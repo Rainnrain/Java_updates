ae58dfa8cffc348fbb66f645ceb794c19a2d7194
Lorraine <lorrainecannom@gmail.com>
2022-08-16 15:04:43 -0400

    Functional Interface practice

--- m3-lambda/src/main/java/com/cydeo/FunctionalInterface.java
+++ m3-lambda/src/main/java/com/cydeo/FunctionalInterface.java
@@ -1,12 +1,38 @@
 package com.cydeo;
 
-import java.util.function.Predicate;
+import java.util.function.*;
 
 public class FunctionalInterface {
     public static void main(String[] args) {
 
-        Predicate<Integer> lesserthan= i->i<18;
+        System.out.println("************        PREDICATE          *************");
 
+        Predicate<Integer> lesserthan= i->i<18;
         System.out.println(lesserthan.test(20));
+
+        System.out.println("************        CONSUMER          *************");
+
+        Consumer<Integer> display= i-> System.out.println(i);
+        display.accept(24);
+
+        System.out.println("************      BI  CONSUMER          *************");
+        BiConsumer<Integer, Integer> addTwo = (x,y) -> System.out.println(x+y);
+        addTwo.accept(3,4);
+
+        System.out.println("************      FUNCTION          *************");
+
+        Function <String, String> fun=s-> "Hello " +s;
+        System.out.println(fun.apply("Cydeo"));
+
+        System.out.println("************    BI  FUNCTION          *************");
+
+        BiFunction<Integer, Integer, Integer> func= (a,b)-> a*b;
+        System.out.println(func.apply(10,2));
+
+        System.out.println("************    SUPPLIER         *************");
+
+        Supplier <Double> randomValue=()->Math.random();
+        System.out.println(randomValue.get());
+
     }
 }
