9cd5340a5260a50c0466f01d1d8f9578fd7add09
Lorraine <lorrainecannom@gmail.com>
2022-08-19 12:00:17 -0400

    Stream

--- m6-stream/src/main/java/com/cydeo/StreamOperations.java
+++ m6-stream/src/main/java/com/cydeo/StreamOperations.java
@@ -6,8 +6,28 @@ import java.util.List;
 public class StreamOperations {
     public static void main(String[] args) {
 
-        List<Integer> list= Arrays.asList(1,2,3,4);
+        List<Integer> list= Arrays.asList(1,2,3,4, 2);
 
-        list.forEach(System.out::println);
+      //  list.forEach(System.out::println);
+        System.out.println("Filter");
+        //Filter
+        list.stream()
+                .filter(i->i%2==0)
+                .forEach(System.out::println);
+
+        //DISTINCT
+        System.out.println("Distinct");
+
+       list.stream()
+               .filter(i->i%2==0)
+               .distinct().forEach(System.out::println);
+
+       //LIMIT
+        System.out.println("Limit");
+
+        list.stream()
+                .filter(i->i%2==0)
+                .limit(1)
+                .forEach(System.out::println);
     }
 }
